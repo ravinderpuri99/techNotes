cf6f134bb30d188db51d21548b96a95a59bab5c2
Ravinder Puri <ravinderpuri99@gmail.com>
2025-02-15 15:51:43 +0530

    Impemented Notes/Users CRUD API in React JS

--- frontend/src/features/notes/EditNoteForm.js
+++ frontend/src/features/notes/EditNoteForm.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useUpdateNoteMutation, useDeleteNoteMutation } from './notesApiSlice'
-import { useNavigate, useSubmit } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSave, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
